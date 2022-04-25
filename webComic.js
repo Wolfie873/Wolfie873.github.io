1e93ba3ad6441212ad021a1c03ff3310fa7a7556
Wolfie873 <jorge.ortiz20@outlook.com>
2022-04-25 17:11:29 -0400

    commit

--- webComic.js
+++ webComic.js
@@ -1,12 +1,7 @@
 window.onload = function () {
-    $("#fwd").click(function () {
-        $img_dir = "Wolfie873.github.io/L&D/";
-        $images = scandir($img_dir);
 
-        console.log('<ul>') ; 
-for($images.length()) 	{ 
-		console.log('<li> <img src=".$img_dir.$img." > </li>');  
-	} 
-console.log('</ul>');
-    });
+    var comics = [""];
+
+    $("#fwd").click(function () {
+        $()
 };
