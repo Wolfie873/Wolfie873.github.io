efa6f744a323ab9f4ad4c17d9ec81b3438110e1f
Wolfie873 <jorge.ortiz20@outlook.com>
2022-05-03 19:09:33 -0400

    commit

--- PHPandTesting/testingsite.php
+++ PHPandTesting/testingsite.php
@@ -19,10 +19,8 @@
       src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
     ></script>
     <!--JQuery input call-->
-    <script src="../Jquery_Practice/jquery-ui-1.13.1/jquery-ui.js"></script>
-    <!--UI elements from JQuery-->
 
-    <link rel="stylesheet" href="mainStyle.css" />
+    <!--<link rel="stylesheet" href="mainStyle.css" />-->
     <!--/*Website Colors:
       --davys-grey: #494947ff;
       --eggshell: #f0ebd8ff;
@@ -30,12 +28,223 @@
       --vermilion: #ca3c25ff;
       --venetian-red: #c20114ff;-->
   </header>
+  
   <body>
-    <?php
-      echo "Hello World!";
+    
+  <div class="container">
+      
+    <div class="row">
+        
+      <div class="col-md-6">
+          
+          <?php
+            echo "<h3>Hello World! </h3>";
 
-      echo "There is so much code YO!";
-    ?>
+            $name = "<h1>Jorge Ortiz de la Mancha</h1>";
+
+            echo "<p>My name is: $name.</p>";
+
+            $string1 = "<p>This is the first.</p>";
+            $string2 = "<p>This is the second.</p>";
+
+            echo $string1.$string2;
+
+            $myNumber = 33;
+            $calculation = $myNumber + 33 / 4 * 6;
+
+            echo "<p>The result of whatever the hell I'm calculating is:</p><p>$calculation</p>";
+
+            $variableName = "name";
+
+            echo $$variableName;
+
+            $myArray = array("Jorge", "Viviana", "Amor");
+
+            print_r($myArray);
+
+            echo "<br><br>";
+
+            echo "<p>Te quiero mucho $myArray[1]</p><br>";
+
+            $anotherArray[0] = "Pizza";
+            $anotherArray[1] = "Hot Dog";
+            $anotherArray[2] = "Banana";
+            $anotherArray[3] = "Margarita";
+            $anotherArray["favoriteFood"] = "Steak";
+
+            print_r($anotherArray);
+
+            echo "<br><br>";
+
+            $thirdArray = array(
+              "PR" => "Spanish", 
+              "USA" => "English", 
+              "FR" => "Français", 
+              "GR" => "Deutsch");
+
+            print_r($thirdArray);
+
+            echo "<br><br>";
+            
+            echo 
+            "<p>The language in USA is ".$thirdArray['USA']."</p>
+            <p>El lenguaje de PR es ".$thirdArray['PR']."</p>
+            <p>La langue en France est le ".$thirdArray['FR']."</p>
+            <p>In Deutschland wird ".$thirdArray['GR']." gesprochen</p>";
+
+            echo "<ul><li>Array 1: "
+            .sizeof($myArray)
+            ."</li><li>Array 2: "
+            .sizeof($anotherArray)
+            ."</li><li>Array 3: "
+            .sizeof($thirdArray)
+            ."</li></ul>";
+
+            $anotherArray[] = "Bitchin'";
+
+            print_r($anotherArray);
+
+            echo "<br><br>";
+
+            unset($anotherArray[1]);
+
+            print_r($anotherArray);
+          ?>
+
+        </div>
+
+        <div class="col-md-6">
+
+          <?php
+
+            $user = "Jj";
+
+            if ($user == "JJ" || $user == "Jj" || $user == "jj") {
+              echo "<h1>Que la que $user!</h1>";
+            }
+            else{
+              echo "<h1 style='color: red; font-size: 100px; font-weight: bold'>Get the fuck out!</h1>";
+            }
+
+            $age = 500;
+
+            if ($age >= 250){
+              echo "<h2> You are exactly $age y/o. Congrats!</h2>";
+            }
+            else{
+              echo "<h2 style='color: red; font-size: 100px'> NOOOOOO </h2>";
+            }
+
+            for ($i = 10; $i >= 0; $i--){
+                echo $i."<br>";
+              }
+
+              $familyArray = array("Jorge", "Oira", "Kim", "Jomar", "Vader");
+
+            for($i = 0; $i < sizeof($familyArray); $i++){
+              echo $familyArray[$i]."<br>";
+            }
+
+            foreach($familyArray as $key => $value){
+
+              $value = $value." Ortiz";
+
+              echo "Array item ".$key." is ".$value."<br>";
+            }
+
+            $farmsize = sizeof($familyArray) - 1;
+            
+
+            while($farmsize >= 0){
+
+              echo "<br>While array item ".$farmsize." is ".$familyArray[$farmsize]."<br>";
+
+              $farmsize = $farmsize - 1;
+            }
+          ?>
+
+        </div>
+
+      </div>
+      <div class="row">
+
+        <div class="col-sm-6">
+
+          <p>Enter a number to see if it's prime: </p>
+
+          <form>
+
+          <input type="text" name="number">
+
+          <input type="submit" name="go">
+
+          </form>
+
+        </div>
+
+        <div class="col-sm-6">
+
+          <?php
+
+          echo "<br><br>";
+
+          print_r($_GET);
+
+          echo "<br><br>";
+
+          $userin = $_GET["number"];
+
+          /*if($userin%2 < $userin && $userin%2 != 0){
+            echo "Prime number.";
+          }
+          else{
+            echo "Composite Number.";
+          }*/
+
+          if(is_numeric($_GET["number"]) && $_GET["number"] > 0 && $_GET["number"] = round($_GET["number"], 0)){
+
+            $i = 2;
+            $isPrime = true;
+
+            while($i < $_GET["number"]){
+              
+              if($_GET["number"] % $i == 0){
+                //Not Prime Number
+                $isPrime = false;
+              }
+              $i++;
+            }
+            
+            if ($isPrime){
+                echo "<h1 style='padding: 10px; color: white; font-weight: bold; background-color: black;'>Congrats! ".$userin." is a prime number!</h1>";
+              }
+              elseif ($isPrime == false){
+                echo "<p style='padding: 10px;'>".$userin." is not a prime number...</p>";
+              }
+          }
+          else if($_GET){
+            echo "<div 
+            class='alert alert-danger alert-dismissible fade show' 
+            role='alert'>
+            <button
+            style='float: right;' 
+            type='button' 
+            class='close' 
+            data-dismiss='alert' 
+            aria-label='Close'> 
+            <span aria-hidden='true'>&times;</span>
+            </button>
+            It needs to be a positive number from 1 to infinity. Hint: 1 and 2 are prime numbers.
+            </div>";
+          }
+
+          ?>
+
+        </div>
+
+    </div>
+      
+  </div>
     <!-- Option 1: Bootstrap Bundle with Popper -->
 
     <script
@@ -44,6 +253,6 @@
       crossorigin="anonymous"
     ></script>
 
-    <script src="coding.js" type="text/javascript"></script>
   </body>
+
 </html>
