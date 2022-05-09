053853e7b1be7de6dfebba589603ab88acbe5d21
Wolfie873 <jorge.ortiz20@outlook.com>
2022-05-09 11:39:58 -0400

    php formatting

--- PHPandTesting/testsite2.php
+++ PHPandTesting/testsite2.php
@@ -7,22 +7,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
   <!-- Bootstrap CSS -->
-  <link 
-  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
-  rel="stylesheet" 
-  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
-  crossorigin="anonymous" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
 
   <!--JQuery input call-->
-  <script 
-    type="text/javascript" 
-    src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
   </script>
   <!--Jquery UI-->
-  <script
-    src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"
-    integrity="sha256-6XMVI0zB8cRzfZjqKcD01PBsAy3FlDASrlC8SxCpInY="
-    crossorigin="anonymous">
+  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js" integrity="sha256-6XMVI0zB8cRzfZjqKcD01PBsAy3FlDASrlC8SxCpInY=" crossorigin="anonymous">
   </script>
 
   <!--<link rel="stylesheet" href="mainStyle.css" />-->
@@ -33,16 +24,16 @@
       --vermilion: #ca3c25ff;
       --venetian-red: #c20114ff;-->
 
-      <style type="text/css">
-      </style>
+  <style type="text/css">
+  </style>
 </header>
 
 <body>
 
   <div class="container">
 
-  <h1>Get In Touch!</h1>
-    
+    <h1>Get In Touch!</h1>
+
     <div class="row">
 
       <div class="alert alert-success" role="alert" id="successalert"></div>
@@ -54,27 +45,27 @@
 
           <fieldset class="form-group">
 
-          <label for="email">E-Mail Here</label>
+            <label for="email">E-Mail Here</label>
 
-          <input type="text" class="form-control" placeholder="email@email.com" id="email" name="email"/>
+            <input type="text" class="form-control" placeholder="email@email.com" id="email" name="email" />
 
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small><br>
-
-          </fieldset>
-
-          <fieldset class="form-group">
-
-          <label for="subject">Subject</label>
-
-          <input type="text" class="form-control" id="subject" name="subject"/><br>
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small><br>
 
           </fieldset>
 
           <fieldset class="form-group">
 
-          <label for="description">What would you like to ask me?</label>
+            <label for="subject">Subject</label>
 
-          <textarea class="form-control" id="description" name="description" rows="10" cols="10"></textarea><br>
+            <input type="text" class="form-control" id="subject" name="subject" /><br>
+
+          </fieldset>
+
+          <fieldset class="form-group">
+
+            <label for="description">What would you like to ask me?</label>
+
+            <textarea class="form-control" id="description" name="description" rows="10" cols="10"></textarea><br>
 
           </fieldset>
 
@@ -89,19 +80,18 @@
   </div>
 
   <script type="text/javascript">
-
     function isEmail(email) {
-        let regexEmail =
-          /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/gim;
-        return regexEmail.test(email);
-      }
+      let regexEmail =
+        /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/gim;
+      return regexEmail.test(email);
+    }
 
-     $(function() {
-    $( "#description" ).resizable();
+    $(function() {
+      $("#description").resizable();
     });
 
-    $("#submit").click(function(){
-      
+    $("#submit").click(function() {
+
       var user = $("#email").val();
 
       var subject = $("#subject").val();
@@ -112,44 +102,41 @@
 
       var failmsg = "";
 
-      if(isEmail(user)){
-        $("#successalert").css("display","block");
+      if (isEmail(user)) {
+        $("#successalert").css("display", "block");
         successmsg += "<p>This is a valid email address</p>";
-      }else{
-        $("#dangeralert").css("display","block");
+      } else {
+        $("#dangeralert").css("display", "block");
         failmsg += "<p>Not a valid email address</p>";
       }
 
-      if(subject != ""){
+      if (subject != "") {
         successmsg += "<p>Subject was not empty</p>";
-      }else{
+      } else {
         failmsg += "<p>No subject was specified</p>";
       }
 
-      if(message != ""){
+      if (message != "") {
         successmsg += "<p>Message field was not empty</p>";
-      }else{
+      } else {
         failmsg += "<p>No message was provided</p>";
       }
 
-      if(successmsg != ""){
+      if (successmsg != "") {
         $("#successalert").css("display", "block");
         $("#successalert").html(successmsg);
       }
-      if(failmsg != ""){
+      if (failmsg != "") {
         $("#dangeralert").css("display", "block");
         $("#dangeralert").html(failmsg);
       }
 
       debugger;
-      });
+    });
   </script>
   <!-- Option 1: Bootstrap Bundle with Popper -->
 
-  <script 
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
-    crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
   </script>
 
 </body>
